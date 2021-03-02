bebc5d3e530e73ef8f89577ff16d8dc0fb881b88
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2021-03-01 21:36:35 -0300

    📌 chore: initial project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
